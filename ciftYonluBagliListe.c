e10bcc051113cff0f0abd945ce866574fd67d928
Mehmet Serdar Dogan <21060620@stu.omu.edu.tr>
2022-10-25 13:30:47 +0300

    fix for çift yonlu liste

--- ciftYonluBagliListe.c
+++ ciftYonluBagliListe.c
@@ -20,7 +20,7 @@ void bastan_yazdir(dugum *bas)
 
 void sondan_yazdir(dugum *kuyruk)
 {
-    dugum *gecici = kuyruk;
+    dugum *gecici = kuyruk->sonraki;
     while (gecici != NULL)
     {
         printf("%d - ", gecici->veri);
@@ -28,17 +28,30 @@ void sondan_yazdir(dugum *kuyruk)
     }
 }
 
-void *dugum_olustur(dugum *kuyruk, int veri)
+void *dugum_olustur(dugum *bas, dugum *kuyruk, int veri)
 {
     dugum *sonuc = malloc(sizeof(dugum));
     sonuc->veri = veri;
     sonuc->sonraki = NULL;
     sonuc->onceki = NULL;
+    kuyruk->sonraki->sonraki = sonuc;
+    if (bas == kuyruk->sonraki)
+    {
+        sonuc->onceki = NULL;
+        kuyruk->sonraki = sonuc;
+    }
+    else
+    {
+        sonuc->onceki = kuyruk->sonraki;
+        kuyruk->sonraki = sonuc;
+    }
 }
 
 int main()
 {
     dugum *bas, *kuyruk;
+    bas = (dugum *)malloc(sizeof(dugum));
+    kuyruk = (dugum *)malloc(sizeof(dugum));
     kuyruk->sonraki = bas;
     int girdi;
     while (1)
@@ -46,14 +59,13 @@ int main()
         printf("Lutfen bir sayi giriniz. (-1 cikis yapar.)\n");
         scanf("%d", &girdi);
         if (girdi == -1)
-        {
             break;
-        }
-        dugum_olustur(kuyruk, girdi);
+        dugum_olustur(bas, kuyruk, girdi);
     }
     bastan_yazdir(bas);
     printf(" \n");
     sondan_yazdir(kuyruk);
+    printf(" \n");
 
     return 0;
 }
