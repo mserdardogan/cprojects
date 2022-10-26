f4c49ada3d0e4c4bdbfb78d42501a8803d56affc
Mehmet Serdar Dogan <21060620@stu.omu.edu.tr>
2022-10-26 21:35:17 +0300

    bir TODO eklendi

--- odev.c
+++ odev.c
@@ -6,19 +6,19 @@ struct dugum
 {
 	char isim[25];
 	char soyisim[25];
-	int yas;
 	long telefonNo;
+	char eposta[320]; // tutarlı bir mail adresinin maksimum uzunluğu 320 olarak belirtilmiş.
 	struct dugum *sonraki;
 };
 typedef struct dugum dugum;
 
-void *dugum_olustur(dugum *kuyruk, char isim[], char soyisim[], int yas, long telefonNo)
+void *dugum_olustur(dugum *kuyruk, char isim[], char soyisim[], long telefonNo, char eposta[])
 {
 	dugum *sonuc = malloc(sizeof(dugum));
 	strcpy(sonuc->isim, isim);
 	strcpy(sonuc->soyisim, soyisim);
-	sonuc->yas = yas;
 	sonuc->telefonNo = telefonNo;
+	strcpy(sonuc->eposta, eposta);
 	sonuc->sonraki = NULL;
 	kuyruk->sonraki->sonraki = sonuc;
 	kuyruk->sonraki = sonuc;
@@ -33,6 +33,29 @@ int main()
 
 	// ilk veriyi girmeden önce geçici olarak kuyruk düğümü baş düğümünü işaret ediyor
 	kuyruk->sonraki = bas;
+	while (1)
+	{
+		printf("Yapmak istediginiz islemi seciniz.\n");
+		printf("-1- Rehbere kisi ekle.\n");
+		printf("-2- Rehberden kisi sil.\n");
+		printf("-3- Kisileri listele.\n");
+		printf("-4- Isme gore sirala.\n");
+		printf("-5- Soyisime gore sirala.\n");
+		printf("-6- Cikis.\n");
+
+		int girdi;
+		scanf("%d", &girdi);
+
+		if (girdi <= 0 || girdi >= 7)
+		{
+			/* code */
+		}
+		else if (girdi == 1)
+		{
+			/* code */
+		}
+		// TODO: while döngüsü tamamlanacak
+	}
 
 	return 0;
 }
