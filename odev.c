e5340e3a544f5cb225b2cf0705eee5a1b696ac34
Mehmet Serdar Dogan <21060620@stu.omu.edu.tr>
2022-10-29 16:21:19 +0300

    hatalar giderildi ve istenen özelliklere kavuştu

--- odev.c
+++ odev.c
@@ -12,7 +12,7 @@ struct dugum
 };
 typedef struct dugum dugum;
 
-void *dugum_olustur(dugum *kuyruk, char isim[], char soyisim[], long telefonNo, char eposta[])
+void dugum_olustur(dugum *kuyruk, char isim[], char soyisim[], long telefonNo, char eposta[])
 {
 	dugum *sonuc = (dugum *)malloc(sizeof(dugum));
 	strcpy(sonuc->isim, isim);
@@ -24,12 +24,110 @@ void *dugum_olustur(dugum *kuyruk, char isim[], char soyisim[], long telefonNo, 
 	kuyruk->sonraki = sonuc;
 }
 
-int main()
+void dugum_sil(dugum *bas, int sira)
+{
+	// oncekiDugum silinecek olan düğümden bir önceki düğümü gösterir
+	dugum *silinecekDugum, *oncekiDugum;
+	oncekiDugum = bas;
+	for (int i = 0; i < sira - 1; i++)
+	{
+		oncekiDugum = oncekiDugum->sonraki;
+	}
+	silinecekDugum = oncekiDugum->sonraki;
+	oncekiDugum->sonraki = oncekiDugum->sonraki->sonraki;
+	free(silinecekDugum);
+}
+
+void butun_listeyi_yazdir(dugum *bas)
+{
+	dugum *gecici = (dugum *)malloc(sizeof(dugum));
+	gecici->sonraki = bas->sonraki;
+	if (gecici->sonraki == NULL)
+	{
+		printf("Rehber bos!\n");
+		return;
+	}
+	else
+	{
+		for (int i = 1; gecici->sonraki != NULL; i++)
+		{
+			printf("%d.\nIsim: %s\nSoyisim: %s\nTelefon Numarası: %ld\nEposta: %s\n\n", i, gecici->sonraki->isim, gecici->sonraki->soyisim, gecici->sonraki->telefonNo, gecici->sonraki->eposta);
+			gecici->sonraki = gecici->sonraki->sonraki;
+		}
+	}
+}
+
+void isimle_ara(dugum *bas, char isim[])
+{
+	dugum *gecici = bas;
+	if (bas->sonraki == NULL)
+	{
+		printf("Rehber bos!\n\n");
+	}
+	else
+	{
+		short i = 0; // boolean gibi çalışacak i değişkeni. eğer while döngüsünde değeri 1 olursa aranan kişi(ler) bulunmuş demektir. 0 ise bulunamamış demektir.
+		while (1)
+		{
+			if (strcmp(bas->sonraki->isim, isim) == 0)
+			{
+				i = 1;
+				printf("\nIsim: %s\nSoyismi: %s\nTelefon numarasi: %ld\nEposta adresi: %s\n\n", bas->sonraki->isim, bas->sonraki->soyisim, bas->sonraki->telefonNo, bas->sonraki->eposta);
+			}
+
+			if (bas->sonraki->sonraki == NULL)
+			{
+				break;
+			}
+
+			bas->sonraki = bas->sonraki->sonraki;
+		}
+		if (i == 0)
+		{
+			printf("Aradiginiz kisi listede yok!\n\n");
+		}
+	}
+	bas = gecici;
+}
+
+void soyisimle_ara(dugum *bas, char soyisim[])
+{
+	dugum *gecici = bas;
+	if (bas->sonraki == NULL)
+	{
+		printf("Rehber bos!\n\n");
+	}
+	else
+	{
+		short i = 0; // boolean gibi çalışacak i değişkeni. eğer while döngüsünde değeri 1 olursa aranan kişi(ler) bulunmuş demektir. 0 ise bulunamamış demektir.
+		while (1)
+		{
+			if (strcmp(bas->sonraki->soyisim, soyisim) == 0)
+			{
+				i = 1;
+				printf("\nIsim: %s\nSoyismi: %s\nTelefon numarasi: %ld\nEposta adresi: %s\n\n", bas->sonraki->isim, bas->sonraki->soyisim, bas->sonraki->telefonNo, bas->sonraki->eposta);
+			}
+
+			if (bas->sonraki->sonraki == NULL)
+			{
+				break;
+			}
+
+			bas->sonraki = bas->sonraki->sonraki;
+		}
+		if (i == 0)
+		{
+			printf("\nAradiginiz kisi listede yok!\n\n");
+		}
+	}
+	bas = gecici;
+}
+
+int main(void)
 {
 	// baş düğümü listenin ilk elemanını, kuyruk ise son elemanı gösterir
 	dugum *bas, *kuyruk;
 	bas = (dugum *)malloc(sizeof(dugum));
-	kuyruk = (dugum *)malloc(sizeof(dugum));
 
 	// ilk veriyi girmeden önce geçici olarak kuyruk düğümü baş düğümünü işaret ediyor
 	kuyruk->sonraki = bas;
@@ -40,8 +138,8 @@ int main()
 		printf("-1- Rehbere kisi ekle.\n");
 		printf("-2- Rehberden kisi sil.\n");
 		printf("-3- Kisileri listele.\n");
-		printf("-4- Isme gore sirala.\n");
-		printf("-5- Soyisime gore sirala.\n");
+		printf("-4- Isimle arama yap.\n");
+		printf("-5- Soyisimle arama yap.\n");
 		printf("-6- Cikis.\n");
 
 		int girdi;
@@ -79,34 +177,37 @@ int main()
 		else if (girdi == 2) // Kişi silme
 		{
 			int sira;
-			dugum *geciciX = (dugum *)malloc(sizeof(dugum));
-			geciciX->sonraki = bas->sonraki; // SAÇMA HATA
-			printf("Silmek istediginiz kisinin rehberdeki sira numarasini giriniz.\n");
-			scanf("%d", &sira);
-			if (sira <= 0)
+			while (1)
 			{
-				// DİKKAT
-				printf("Lutfen bir sayma sayisi girin.\n");
+				printf("Silmek istediginiz kisinin rehberdeki sira numarasini giriniz.\n");
+				scanf("%d", &sira);
+				if (sira <= 0)
+				{
+					printf("Lutfen bir sayma sayisi girin.\n");
+					continue;
+				}
+				dugum_sil(bas, sira);
+				break;
 			}
 		}
 		else if (girdi == 3) // Kişileri listeleme
 		{
-			dugum *gecici;
-			gecici->sonraki = bas->sonraki;
-			int i = 1;
-			while (gecici->sonraki != NULL)
-			{
-				printf("%d.\nIsim: %s\nSoyisim: %s\nTelefon Numarası: %ld\nEposta: %s\n\n", i, gecici->sonraki->isim, gecici->sonraki->soyisim, gecici->sonraki->telefonNo, gecici->sonraki->eposta);
-				i++;
-				gecici->sonraki = gecici->sonraki->sonraki;
-			}
+			butun_listeyi_yazdir(bas);
 		}
-		// else if (girdi == 4)
-		//{
-		// }
-		// else if (girdi == 5)
-		//{
-		// }
+		else if (girdi == 4) // isimle arama yap
+		{
+			char arananIsim[25];
+			printf("Aramak istediginiz kisinin ismini giriniz:\n");
+			scanf("%s", &arananIsim);
+			isimle_ara(bas, arananIsim);
+		}
+		else if (girdi == 5) // soyisimle arama
+		{
+			char arananSoyisim[25];
+			printf("Aramak istediginiz kisinin soyismini giriniz:\n");
+			scanf("%s", &arananSoyisim);
+			soyisimle_ara(bas, arananSoyisim);
+		}
 		else if (girdi == 6)
 		{
 			break;
